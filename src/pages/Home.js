6d8b89c3d2d295e0472c990fc5f613aa0e9ee100
smishra11 <msubhasish11@gmail.com>
2020-06-04 19:47:00 +0530

    Added code for services section in the project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
+import Services from '../components/Services';
 
 const Home = () => {
   return(
-    <Hero>
-      <Banner title="Luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <React.Fragment>
+      <Hero>
+        <Banner title="Luxurious rooms" subtitle="deluxe rooms starting at $299">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </React.Fragment>
   )
 }
 
